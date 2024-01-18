d616e36c4be16eba24b363a25f1a37a992b08997
Lindanzavi <lindanzavi@gmail.com>
2024-01-17 17:10:13 -0800

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,26 +14,5 @@ int checker(int argc, int i, unsigned int j, char *argv[])
 		for (j = 0; argv[i] != '\0' && j < strlen(argv[i]); j++)
 			if (isdigit(argv[i][j]) == 0)
 				return (1);
-	return (0);
-}
-/**
- * main - adds all arguments together if they are digits.
- * @argc: argument count only accepts ints separated by spaces.
- * @argv: argument vector
- * Return: 0 on success, 1 on failure
- */
-int main(int argc, char *argv[])
-{
-	int result, i;
-
-	result = 0;
-	if (checker(argc, 1, 0, argv) == 1)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	for (i = 1; i < argc; i++)
-		result += atoi(argv[i]);
-	printf("%d\n", result);
-	return (0);
+	return (0)
 }
